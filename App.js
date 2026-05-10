beaccec31e97383efd067f13145b9127745fccdb
Jesus Jimenez <jimenezmartinezjesus76@gmail.com>
2026-05-10 12:37:26 -0600

    fix: StatusBar forzado light-content en App.js y app.json, backgroundColor android #020509

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: '#020509' }}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="#020509"
+        translucent={false}
+      />
       <AppNavigator />
     </View>
   );
